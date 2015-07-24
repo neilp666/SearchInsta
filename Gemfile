35048508c6079d1164ea2b65d2e442a92b6e970a
neil patel <neil@myindianweddingapp.com>
2015-07-24 19:23:05 +0100

    Updated styling on home page

--- Gemfile
+++ Gemfile
@@ -45,5 +45,5 @@ gem 'figaro'
 # gem 'capistrano-rails', group: :development
 
 # Use debugger
-# gem 'debugger', group: [:development, :test]
+gem 'byebug', group: [:development, :test]
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,35 @@
 class PagesController < ApplicationController
   def home
     access_token = ENV["CONFIG.ACCESS_TOKEN"]
+    #Access token identifies the user, 
+ 
     client = Instagram.client(access_token: access_token)
     
+  
     default_search = client.tag_search('travel')
+   
 
-    if params[:q]
+    if params[:q] 
       search_query = client.tag_search(params[:q])
       @tag = search_query.present? ? search_query : default_search
+
     else 
       @tag = default_search 
     end
 
     @tag = @tag.first.name
     @search = client.tag_recent_media(@tag)
+
+   
+
   end
 
   def about
   end
   
 end
+
+#searching for a default tag and if user proviedes another tag
+#it searches for that tag. Then it will look for the first item of the result 
+#and i ask for the tag recent media for that name.
+
